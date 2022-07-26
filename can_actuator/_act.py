62b4dc8476554234910ce3e0a9216383fc9fbcda
Seif Farag <seif.abdelrhman60@gmail.com>
2022-07-26 17:11:18 +0300

    Script Update
    updated the actuator script to test the hardware

--- can_actuator/_act.py
+++ can_actuator/_act.py
@@ -7,7 +7,7 @@ can = CAN(1, CAN.NORMAL, extframe=False, prescaler=2, sjw=1, bs1=14, bs2=6)
 can.setfilter(0, CAN.LIST16, 0, (320, 321, 322, 323))
 
 act1 = MyActuator(device_id=321, can=can)
-act2 = MyActuator(device_id=322, can=can)
+# act2 = MyActuator(device_id=322, can=can)
 
 te = 0
 tp = 0
@@ -20,9 +20,10 @@ while True:
         # can.send(command, 0x141)   # seWnd a message with id 123
         t1 = ticks_us()
         state_1 = act1.set_torque(0)
-        state_2 = act2.set_torque(0)
+        # state_2 = act2.set_torque(0)
         t2 = ticks_us()
 
     if t - tp > 20000:
         tp = t
-        print(t2 - t1, state_1, state_2)
+        # print(t2 - t1, state_1, state_2)
+        print(t2 - t1, state_1)
